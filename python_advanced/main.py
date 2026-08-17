b3b602d0dda5f33725e2a6453e4fbda1e7e9fa0b
Andrew Robert Sutton <53604048+ARS-Technica@users.noreply.github.com>
2026-08-16 21:08:31 -0400

    Add Project Description per Project Requirements

--- python_advanced/main.py
+++ python_advanced/main.py
@@ -1,3 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Code the Dream 
+Advanced Python Prework
+
+Project: Higher or Lower Game
+    
+Created on Monday August 15 2026
+
+"A user runs the program and is prompted to select a region name. The program 
+fetches all countries from the countries.dev API, filters to those in the 
+matching region, and populates a dictionary with each country’s name and 
+population.  The program then selects two of the countries at random and asks 
+the user which one has the larger population. If the user chooses correctly, 
+their score increments upward. If the user chooses incorrently three times, the 
+game ends.
+
+Try to beat your previous high score!"
+"""
+
+
 import json
 import requests
 
